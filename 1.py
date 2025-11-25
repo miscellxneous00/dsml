8e05713da189bd11d541572ef24f6c829ac31350
miscellxneous00 <miscellxneous00@gmail.com>
2025-11-25 10:27:46 +0530

    read data from different formats,indexing ,selecting data, sort data, describe attributes of data, data types of column.

--- 1.py
+++ 1.py
@@ -4,6 +4,7 @@
 #where python3
 #which python3
 
+
 #Perform the following operations using Python on a data set : read data from different formats(like csv, xls),indexing and selecting data, sort data, describe attributes of data, checking data types of each column. (Use Titanic Dataset)
 
 # 1. Import required libraries
