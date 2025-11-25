da019be2b8d60f763854abf950eac07fc4cc3807
miscellxneous00 <miscellxneous00@gmail.com>
2025-11-25 09:34:30 +0530

    Update 15_16.py

--- 15_16.py
+++ 15_16.py
@@ -1,3 +1,7 @@
+#Use the dataset 'titanic'. The dataset contains 891 rows and contains
+#information about the passengers who boarded the unfortunate Titanic
+#ship. Use the Seaborn library to see if we can find any patterns in the data.
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
