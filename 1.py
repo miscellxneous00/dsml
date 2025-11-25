721dac3e42144000fb81cec2ba61f09f3eec81a6
miscellxneous00 <miscellxneous00@gmail.com>
2025-11-25 09:23:12 +0530

    Update 1.py
    Perform the following operations using Python on a data set : read data
    from different formats(like csv, xls),indexing and selecting data, sort data,
    describe attributes of data, checking data types of each column. (Use
    Titanic Dataset)

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
 #python --version
 #pip install pandas
 # python3 -m pip install pandas
+#where python3
+#which python3
 
 #Perform the following operations using Python on a data set : read data from different formats(like csv, xls),indexing and selecting data, sort data, describe attributes of data, checking data types of each column. (Use Titanic Dataset)
 
