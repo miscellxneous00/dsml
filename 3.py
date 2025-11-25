d48ac29af3068c68007acc7e5531f6ed94072936
miscellxneous00 <miscellxneous00@gmail.com>
2025-11-25 09:35:28 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,3 +1,8 @@
+# Perform the following operations using Python on the data set
+# House_Price Prediction dataset. Compute standard deviation, variance and
+# percentiles using separate commands, for each feature. Create a histogram
+# for each feature in the dataset to illustrate the feature distributions
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
